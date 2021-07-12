6b3fec1b1cdb7a27e758b71d27df7912d81cfe12
Brandon Knox <bknox.dev@gmail.com>
2021-07-12 14:17:34 -0700

    Example of switching between horizontal and vertical layouts for content, triggered by tap gesture

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -7,23 +7,34 @@
 
 import SwiftUI
 
-struct User: Identifiable {
-    var id = "Taylor Swift"
+struct UserView: View {
+    var body: some View {
+        Group {
+            Text("Name: Paul")
+            Text("Country: England")
+            Text("Pets: Luna, Arya, and Toby")
+        }
+    }
 }
 
 struct ContentView: View {
-    @State private var selectedUser: User? = nil
-    @State private var isShowingAlert = false
+    @State private var layoutVertically = false
     
     var body: some View {
-        Text("Hello, world!")
-            .onTapGesture {
-                self.selectedUser = User()
-                self.isShowingAlert = true
+        Group {
+            if layoutVertically {
+                VStack {
+                    UserView()
+                }
+            } else {
+                HStack {
+                    UserView()
+                }
             }
-            .alert(isPresented: $isShowingAlert) {
-                Alert(title: Text(selectedUser!.id))
-            }
+        }
+        .onTapGesture {
+            self.layoutVertically.toggle()
+        }
     }
 }
 
