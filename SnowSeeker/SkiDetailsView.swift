90ff3018955776668016495c7078dbd81a729c61
Brandon Knox <bknox.dev@gmail.com>
2021-07-13 19:12:08 -0700

    Added ResortView, ResortDetailsView, and SkiDetailsView.  Content view modified to point to ResortView.

---
+++ SnowSeeker/SkiDetailsView.swift
@@ -0,0 +1,25 @@
+//
+//  SkiDetailsView.swift
+//  SnowSeeker
+//
+//  Created by Brandon Knox on 7/13/21.
+//
+
+import SwiftUI
+
+struct SkiDetailsView: View {
+    let resort: Resort
+    
+    var body: some View {
+        VStack {
+            Text("Elevation: \(resort.elevation)m")
+            Text("Snow: \(resort.snowDepth)cm")
+        }
+    }
+}
+
+struct SkiDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SkiDetailsView(resort: Resort.example)
+    }
+}
