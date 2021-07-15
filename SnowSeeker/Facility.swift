7fb79ff2255b79f0cbc45415d924280e315f82dd
Brandon Knox <bknox.dev@gmail.com>
2021-07-14 18:16:43 -0700

    Created Favorites class as an observable object to track resorts users have selected as favorites, along with some stubs for loading/saving this data.  Button added to ResortView to mark favorites.

--- SnowSeeker/Facility.swift
+++ SnowSeeker/Facility.swift
@@ -34,7 +34,7 @@ struct Facility: Identifiable {
             "Accommodation": "This resort has popular on-site accommodation",
             "Beginners": "This resort has lots of ski schools",
             "Cross-country": "This resort has many cross-country ski routes",
-            "Eco-friendy": "This resort has won an award for environmental friendliness",
+            "Eco-friendly": "This resort has won an award for environmental friendliness",
             "Family": "This resort is popular with families"
         ]
         
